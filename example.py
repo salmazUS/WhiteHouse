6d8aa877193d6f3a36c2539e5ba54ba25415b3c7
uzma <uzmasalamaz2015@gmail.com>
2023-04-10 22:46:58 +0530

    Uzma ka first project

---
+++ example.py
@@ -0,0 +1,26 @@
+print("My First Python project")
+print("I am arish.My First Python project")
+print("I am danish. My First Python project")
+a = 10, 20, 2
+b = 20, 25, 1
+c = a+b
+print("sum =", c)
+if b > a:
+    print("correct")
+else:
+    print("incorrect")
+if c == 30:
+    print("c =", c)
+    print("correct")
+name = input("what is your name? ")
+print("Hello " + name)
+a=20
+b=20.1
+c=a+b
+print(c)
+First = input("First number = ")
+Second = input("Second number = ")
+Sum = int(First) + float(Second)
+print("sum = ", Sum)
+
+
